f63c8ab9dd37b8fa454952eb1b9612fa2974e710
gabrielow000 <gabrielrafaelsanchezrodriguez@gmail.com>
2022-04-11 11:48:50 -0400

    CRUD y Login

---
+++ js/5194.056905d8.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunktest"]=self["webpackChunktest"]||[]).push([[5194],{5194:(e,t,n)=>{n.r(t),n.d(t,{default:()=>f});var s=n(3673);function a(e,t){const n=(0,s.up)("q-page");return(0,s.wg)(),(0,s.j4)(n,{padding:"",id:"Contacts"})}var c=n(4260),r=n(4379),u=n(7518),p=n.n(u);const d={},o=(0,c.Z)(d,[["render",a]]),f=o;p()(d,"components",{QPage:r.Z})}}]);
